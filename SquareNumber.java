c8e4f5a016a2119f0a202621ad215842e83db8a6
Chhotu kumar <168346582+Chhotu2003@users.noreply.github.com>
2025-07-13 18:06:54 +0000

    Square Number using in java code return method based

---
+++ SquareNumber.java
@@ -0,0 +1,12 @@
+public class SquareNumber {
+    public static int SqurtNum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n*n;  
+     }
+     public static void main(String[] args) {
+        int n=5;
+        System.out.println(SqurtNum(n));
+     }
+}
